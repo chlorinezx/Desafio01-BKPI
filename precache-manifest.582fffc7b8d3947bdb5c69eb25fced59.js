ff721fe6736f5c695bea7ee093a30f80bb1d4239
chlronizx <gabriellglma9@gmail.com>
2022-06-14 18:51:43 -0300

    Updates

---
+++ precache-manifest.582fffc7b8d3947bdb5c69eb25fced59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d73291cf7d4d2fcc9d",
+    "url": "/chlorinezx/static/css/main.2d944864.chunk.css"
+  },
+  {
+    "revision": "f2d73291cf7d4d2fcc9d",
+    "url": "/chlorinezx/static/js/main.f2d73291.chunk.js"
+  },
+  {
+    "revision": "0d5d006d44bc40c6abc2",
+    "url": "/chlorinezx/static/js/1.0d5d006d.chunk.js"
+  },
+  {
+    "revision": "5d0479bea1310a9a09da",
+    "url": "/chlorinezx/static/js/runtime~main.5d0479be.js"
+  },
+  {
+    "revision": "34c2b9b5537efdce3055190c3af59034",
+    "url": "/chlorinezx/static/media/bulbasaur.34c2b9b5.png"
+  },
+  {
+    "revision": "36c38a9a8949757bc6d99d05fab36906",
+    "url": "/chlorinezx/static/media/ivysaur.36c38a9a.png"
+  },
+  {
+    "revision": "5b52650a77c361446f3ba0e3574c46cf",
+    "url": "/chlorinezx/static/media/venusaur.5b52650a.png"
+  },
+  {
+    "revision": "f295769d9bd3c34ffc552e837f5304ae",
+    "url": "/chlorinezx/static/media/charmander.f295769d.png"
+  },
+  {
+    "revision": "b6868679ea8d6b5565fbeacd9669de0a",
+    "url": "/chlorinezx/static/media/charmeleon.b6868679.png"
+  },
+  {
+    "revision": "7d327a3f5b3d6804c5ae9d1d3bed1ec9",
+    "url": "/chlorinezx/static/media/charizard.7d327a3f.png"
+  },
+  {
+    "revision": "6a55d8b31614b5d2726256b3982a46ab",
+    "url": "/chlorinezx/static/media/logo.6a55d8b3.png"
+  },
+  {
+    "revision": "dd97c260677a6689ac37b66fc81e2cbe",
+    "url": "/chlorinezx/index.html"
+  }
+];
